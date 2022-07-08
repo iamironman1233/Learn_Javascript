2171965c7bed6a444a362e14eb8a9658f522173f
Son Dinh Thai <87920408+iamironman1233@users.noreply.github.com>
2022-07-09 04:01:06 +0700

    Create 9.js

---
+++ 9.js
@@ -0,0 +1,25 @@
+function run(datas){
+	var a = 12;
+	
+	var b = "javascript";
+
+	var c = true;
+
+	var d = ['Apple', 'Banana'];
+
+	var e = {type:"Fiat", model:"500", color:"white"};
+
+	var f = function() {
+		console.log("javascript");
+	};
+
+	var g;
+	
+	console.log(typeof 12);
+	console.log(typeof 'Javascript');
+	console.log(typeof true);
+	console.log(typeof ["Apple", "Banana"]);
+	console.log(typeof {"type":"Fiat", "model":"500", "color":"white"});
+	console.log(typeof Function);
+	console.log(typeof declaredButUndefinedVariable);
+}
