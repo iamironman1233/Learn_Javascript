0dc2658f67a93fe15fbf51e3967b42234f52f904
Son Dinh Thai <87920408+iamironman1233@users.noreply.github.com>
2022-07-09 03:54:57 +0700

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,10 @@
+function run() {
+	var gfor = "15";
+	console.log(gfor);
+	var g123 = "15";
+	console.log(g123);
+	var gbreak = "15";
+	console.log(gbreak);
+	var greturn = 15;
+	console.log(greturn);
+}
